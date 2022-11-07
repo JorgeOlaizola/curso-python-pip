7eb7b5de340022ff7e351156f9015c89495e054b
Jorge Olaizola <jorgelolaizola@hotmail.com>
2022-11-07 14:40:53 -0300

    updated views in products app

--- django-app/prometeocoleccionablesapp/products/urls.py
+++ django-app/prometeocoleccionablesapp/products/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path 
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("categories/<int:category_id>",
+         views.detail_category, name='category_detail'),
+    path('<int:product_id>', views.detail_product, name='product_detail'),
+    path('<int:product_id>/add_stock', views.add_stock, name='add_stock')
 ]
--- django-app/prometeocoleccionablesapp/products/views.py
+++ django-app/prometeocoleccionablesapp/products/views.py
@@ -3,5 +3,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("Estás en la página principal de la aplicación /products/")
+
+
+def detail_category(request, category_id):
+    return HttpResponse(f"Estás viendo la categoría número {category_id}")
+
+
+def detail_product(request, product_id):
+    return HttpResponse(f"Estás viendo el producto número {product_id}")
+
+
+def add_stock(request, product_id):
+    return HttpResponse(f"Estás agregando stock al producto número {product_id}")
