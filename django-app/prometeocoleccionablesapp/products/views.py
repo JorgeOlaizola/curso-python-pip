98cdbcb73dd27c43ba8bf842d8a06219692b4f3d
Jorge Olaizola <jorgelolaizola@hotmail.com>
2022-11-09 11:38:34 -0300

    Added template/views

--- django-app/prometeocoleccionablesapp/products/views.py
+++ django-app/prometeocoleccionablesapp/products/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Product, Category
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Estás en la página principal de la aplicación /products/")
+    latest_product_list = Product.objects.all()
+    return render(request, "products/index.html", {"latest_product_list": latest_product_list})
 
 
 def detail_category(request, category_id):
@@ -13,7 +15,8 @@ def detail_category(request, category_id):
 
 
 def detail_product(request, product_id):
-    return HttpResponse(f"Estás viendo el producto número {product_id}")
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, "products/detail.html", {"product": product})
 
 
 def add_stock(request, product_id):
